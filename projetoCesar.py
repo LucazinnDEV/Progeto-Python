4765477aa4f899ac4b8780d478b385dcb09c61f2
Lucas Samuel <lucasdevctt01@gmail.com>
2024-11-20 19:16:00 -0300

    Adicionamos a função menu

--- projetoCesar.py
+++ projetoCesar.py
@@ -8,3 +8,17 @@ def inicializar () :
             case "2" :
                 visualizar()
     
+def menu () :
+    print ("==== Bem vindo ao menu! ====")
+    print ("1 - Adicionar treino.")
+    print ("2 - Visualisar treinos.")
+    print ("3 - Atualizar treinos.")
+    print ("4 - Filtrar treino.")
+    print ("5 - Excluir treinos.")
+    print ("6 - Definir metas.")
+    print ("7 - Sujestões de treinos.")
+    print ("8 - Sair.")
+    return input("Escolha uma opção: ")
+
+menu()
+
